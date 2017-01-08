997730ff475012ecb44963e92f6f42d2e4046be7
Mike Golubitsky <mgolubitsky@solarcity.com>
2017-01-07 17:43:22 -0800

    add react-redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 import gameOfLifeApp from './reducers/App';
 import App from './components/App';
 import './index.css';
